dc97a4a7a8e45c82ce1ed7b0be43f509f0df1f7e
saradonnelly97 <sara.donnelly8@gmail.com>
2023-05-24 22:23:07 -0400

    Added pricing

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -37,48 +37,48 @@ function getProductInfo(paintColor) {
     
     switch (paintColor) {
         case 'pn1':           
-            price = '$9.99' 
+            price = '$14.99' 
             colorName = 'Gloop Green'
             updatePrice(colorName, price) // set variables for price and color name and invoke a function to update the price     
             break;           
         case 'pn2':
-            price = '$13'
+            price = '$11.14'
             colorName = 'Benson Brown'
             updatePrice(colorName, price)   
             break;            
         case 'pn3':
-            price = '$13'
-            colorName = 'Whatever I wanted'
+            price = '$22.99'
+            colorName = 'Argyle Azul'
             updatePrice(colorName, price)      
             break;   
         case 'pn4':
-            price = '$13'
-            colorName = 'Whatever I wanted'
+            price = '$4.99'
+            colorName = 'Really Rad Red'
             updatePrice(colorName, price)      
             break;   
         case 'pn5':
-            price = '$13'
-            colorName = 'Whatever I wanted'
+            price = '$8.22'
+            colorName = 'VANILLA.'
             updatePrice(colorName, price)           
             break;   
         case 'pn6':
-            price = '$13'
-            colorName = 'Whatever I wanted'
+            price = '$11.99'
+            colorName = 'Shadowy Shadow'
             updatePrice(colorName, price)             
             break;   
         case 'pn7':
-            price = '$13'
-            colorName = 'Whatever I wanted'
+            price = '$13.42'
+            colorName = 'Waaaaaater'
             updatePrice(colorName, price)   
             break;    
         case 'pn8':
-            price = '$13'
-            colorName = 'Whatever I wanted'
+            price = '$21.98'
+            colorName = 'Royalty Free'
             updatePrice(colorName, price)   
             break;   
         case 'pn9':
-            price = '$13'
-            colorName = 'Whatever I wanted'
+            price = '$14.99'
+            colorName = 'Sunshine! Sunshine! Sunshine!'
             updatePrice(colorName, price)   
             break;    
           default:              
